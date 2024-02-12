dcf603e3a95473d69c8289c24f1270882ebdf44e
vanced1603 <155910866+vanced1603@users.noreply.github.com>
2024-02-12 09:58:53 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -34,6 +34,12 @@ def mongraphique():
 @app.route("/histogramme/")
 def monhistogramme():
     return render_template("histogramme.html")
+
+@app.route('/extract-minutes/<date_string>')
+def extract_minutes(date_string):
+        date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+        minutes = date_object.minute
+        return jsonify({'minutes': minutes})
   
 if __name__ == "__main__":
   app.run(debug=True)
